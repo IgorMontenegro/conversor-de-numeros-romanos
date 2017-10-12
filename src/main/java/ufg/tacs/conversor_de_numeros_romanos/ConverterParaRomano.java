6f75555bc6e85ba705ba72f710c6c5c3b48fa104
Unknown <c.montenegro.c@hotmail.com>
2017-10-11 22:07:15 -0300

    Conversor de número romano
    Teste de unidade Junit 90% 
    Projeto Mavem

---
+++ src/main/java/ufg/tacs/conversor_de_numeros_romanos/ConverterParaRomano.java
@@ -0,0 +1,101 @@
+package ufg.tacs.conversor_de_numeros_romanos;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ConverterParaRomano
+{	
+	//Mapa de tradução de números romanos para decimais
+	private Map<String, Integer> enumeradorRomanos;
+	
+	public Integer converter(String numeroRomano) {
+		
+		Integer numeroDecimal = null;
+		iniciarTabelaDeConversao();
+		
+		if(numeroRomano != null && !numeroRomano.isEmpty() && validarNumerosRomanos(numeroRomano) ){
+			numeroRomano = numeroRomano.toUpperCase();
+			numeroDecimal = enumeradorRomanos.get(numeroRomano);
+
+			if(numeroDecimal == null){
+				numeroDecimal = 0;
+				
+				int i = 0;
+				
+				while (i < numeroRomano.length()) {
+			          
+		             char letraRomano = numeroRomano.charAt(i);
+		             String stringLetraRomano = letraRomano + "";
+		             
+		             if(enumeradorRomanos.containsKey(stringLetraRomano)) {
+			             int numeroAtual = enumeradorRomanos.get(stringLetraRomano); 
+			             i++; 
+			             
+			             if (i == numeroRomano.length()) {
+			                
+			            	 numeroDecimal += numeroAtual;
+			             }
+			             else {
+			            	 char letraProximoRomano = numeroRomano.charAt(i); 
+			            	 String proximoRomano = letraProximoRomano + "";
+			            	 
+			                int numeroProximo = enumeradorRomanos.get(proximoRomano);
+			                if (numeroProximo > numeroAtual) {
+			                	numeroDecimal += (numeroProximo - numeroAtual);
+			                	i++;
+			                }
+			                else {
+			                	numeroDecimal += numeroAtual;
+			                }
+			             }
+		             } 
+		          }   
+			}
+			
+			return numeroDecimal;
+		}
+		return numeroDecimal;
+	}
+
+	private void iniciarTabelaDeConversao() {
+		enumeradorRomanos = new HashMap<String, Integer>();
+		enumeradorRomanos.put("I", 1);
+		enumeradorRomanos.put("II", 2);
+		enumeradorRomanos.put("III", 3);
+		enumeradorRomanos.put("IV", 4);
+		enumeradorRomanos.put("V", 5);
+		enumeradorRomanos.put("VI", 6);
+		enumeradorRomanos.put("VII", 7);
+		enumeradorRomanos.put("VIII", 8);
+		enumeradorRomanos.put("IX", 9);
+		enumeradorRomanos.put("X", 10);
+		enumeradorRomanos.put("L", 50);
+		enumeradorRomanos.put("C", 100);
+		enumeradorRomanos.put("D", 500);
+		enumeradorRomanos.put("M", 1000);
+		
+	}
+
+	private boolean validarNumerosRomanos(String romano) {
+		boolean resultado = false;
+		
+		romano = romano.toUpperCase();
+		
+		/*A pedido do professor, a notaÃ§Ã£o do nÃºmero 4000 foi mudada para MMMM, pois nÃ£o Ã© possivel comparar a notaÃ§Ã£o 
+		original romana.*/
+		
+		Pattern pattern = Pattern.compile("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+		Matcher matcher = pattern.matcher(romano);
+		boolean regexValido = matcher.matches();
+		
+		if(regexValido){
+			resultado = true;
+		}
+		
+		return resultado;
+	}
+}
+
+
---
+++ src/test/java/ufg/tacs/conversor_de_numeros_romanos/ConverterPararRomanoTest.java
@@ -0,0 +1,38 @@
+package ufg.tacs.conversor_de_numeros_romanos;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class ConverterPararRomanoTest extends TestCase {
+
+	@Test
+	public void testRomanosTraduziveis() {
+		ConverterParaRomano conversor = new ConverterParaRomano();
+		
+		assertEquals(new Integer(1), conversor.converter("i"));
+		assertEquals(new Integer(9), conversor.converter("ix"));
+		assertEquals(new Integer(30), conversor.converter("xxx"));
+		assertEquals(new Integer(1280), conversor.converter("MCCLXXX"));
+		assertEquals(new Integer(4974), conversor.converter("MMMMCMLXXIV"));
+	}
+	
+	@Test
+	public void testEntradasInvalidas() {
+		ConverterParaRomano conversor = new ConverterParaRomano();
+
+		assertEquals(null, conversor.converter("0"));
+		assertEquals(null, conversor.converter("ABC"));
+		assertEquals(null, conversor.converter(null));
+		assertEquals(null, conversor.converter(""));
+		
+	}
+	
+	@Test
+	public void testRomanosIntraduziveis() {
+		ConverterParaRomano conversor = new ConverterParaRomano();
+		assertEquals(null, conversor.converter("xiiii"));
+		assertEquals(null, conversor.converter("xxxx"));
+	}
+	
+}
